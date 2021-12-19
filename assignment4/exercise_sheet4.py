8bb444314086565e202b9286b6fa44f7bade8a7b
riccardo preite <riccardo.preite.10@gmail.com>
2021-12-19 14:13:03 +0100

    up

--- assignment4/exercise_sheet4.py
+++ assignment4/exercise_sheet4.py
@@ -12,7 +12,11 @@ parser.add_argument('--lr', type=float, nargs='?',
 parser.add_argument('--test', type=str, nargs='?',
                     help='file of the model to load')
 parser.add_argument('--mode', type=str, nargs='?',
-                    help='model to test. "full" test on corpus_pos.txt, "slice" test on 50 sentence of corpus_pos.txt, "example" test on corpus_example.txt, "obama" test on corpus_obama. Default slice')
+                    help='model to test. "full" test on corpus_pos.txt, "slice" test on 50 (default) sentence of corpus_pos.txt, "example" test on corpus_example.txt, "obama" test on corpus_obama. Default slice')
+parser.add_argument('--ite', type=int, nargs='?',
+                    help='Number of itearation for the training. Default 20')
+parser.add_argument('--size', type=int, nargs='?',
+                    help='Train size. Default 50')
 
 '''
 This function can be used for importing the corpus.
@@ -41,71 +45,71 @@ def import_corpus(path_to_file):
     f.close()
     return sentences
 
-def obama(line: int, learning_rate: float):
+def obama(line: int, learning_rate: float, iteration_number:int, size:int):
     print("Training on obama corpus")
     corpus = import_corpus('corpus_obama.txt')
 
     model = LinearChainCRF()
     model.initialize([corpus[1]])
 
-    model.train(20, learning_rate)
-    save("obama", learning_rate, model)
+    model.train(iteration_number, learning_rate)
+    save("obama", learning_rate, model,1)
 
     print("\n", model.most_likely_label_sequence(corpus[0]))
     print("\n",corpus[0])
 
-def example(line: int, learning_rate: float):
+def example(line: int, learning_rate: float, iteration_number:int, size:int):
     print("Training on example corpus")
     corpus = import_corpus('corpus_example.txt')
 
     model = LinearChainCRF()
-    model.initialize(corpus[:2])
+    model.initialize(corpus[:2],2)
 
-    model.train(20, learning_rate)
+    model.train(iteration_number, learning_rate)
     save("example", learning_rate, model)
 
     print("\n", model.most_likely_label_sequence(corpus[2]))
     print("\n",corpus[2])
 
-def slice(line: int, learning_rate: float):
-    print("Training on slice corpus")
+def slice(line: int, learning_rate: float, iteration_number:int, size: int):
+    print("Training on slice corpus of size",size)
     corpus_full = import_corpus('corpus_pos.txt')
     corpus = []
-    if line != 0:
-        for i in range(len(corpus)):
-            if i != line:   
-                corpus.append(corpus_full[i])
-    else:
-        corpus = corpus_full[1:50]
+    i = 0
+    while len(corpus) < size:
+        if i != line:   
+            corpus.append(corpus_full[i])
+        i+=1
     
     model = LinearChainCRF()
     model.initialize(corpus)
 
-    model.train(20, learning_rate)
-    save("slice", learning_rate, model)
+    model.train(iteration_number, learning_rate)
+    save("slice", learning_rate, model, len(corpus))
     print("\n", model.most_likely_label_sequence(corpus[line]))
     print("\n",corpus[line])
 
-def full(line: int, learning_rate: float):
-    print("Training on full corpus")
+def full(line: int, learning_rate: float, iteration_number:int, size: int):
+    
     corpus_full = import_corpus('corpus_pos.txt')
     test_sentence = corpus_full.pop(line)
+    print("Training on full corpus of size ",corpus_full)
     
     model = LinearChainCRF()
     model.initialize(corpus_full)
 
-    model.train(20, learning_rate)
-    save("full", learning_rate, model)
+    model.train(iteration_number, learning_rate)
+    save("full", learning_rate, model, len(corpus_full))
     print("\n", model.most_likely_label_sequence(test_sentence))
     print("\n",test_sentence)
 
-def save(mode:str, learning_rate: float, model):
+def save(mode:str, learning_rate: float, model, size: int):
         exist = os.path.exists("models")
         if not exist:
             os.makedirs("models")
 
         date:str = datetime.datetime.now().strftime("day_%Y-%m-%d_time_%H-%M-%S_")
-        name = "models/"+mode+"_"+date+'_lr'+str(learning_rate)+".pkl"
+        name = "models/"+mode+"_"+date+'_lr_'+str(learning_rate)+"_train_size_"+size+".pkl"
         print(model)
         pickle.dump(model, open( name, "wb" ),-1)
 
@@ -126,7 +130,8 @@ if __name__ == "__main__":
     line = args.line if args.line != None else 0
     learning_rate = args.lr if args.lr != None else 0.01
     mode = args.mode if args.mode != None else "slice"
-    
+    iteration_number = args.ite if args.ite != None else 20
+    size = args.size if args.size != None else 50
     
     if args.test != None:
         '''Test'''
@@ -136,7 +141,7 @@ if __name__ == "__main__":
         test(model, line)
     else:
         '''Train'''
-        function[mode](line, learning_rate)
+        function[mode](line, learning_rate, iteration_number, size)
     
     
 
